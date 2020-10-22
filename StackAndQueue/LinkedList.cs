9707e56c5fa907869eb30c1f1f522c895998f476
piyush12346789 <piyushgupta024@gmail.com>
2020-10-22 19:34:06 +0530

    [Piyush] Add: Method to peek and pop from the stack using linkedlist.

--- StackAndQueue/LinkedList.cs
+++ StackAndQueue/LinkedList.cs
@@ -12,7 +12,7 @@ namespace StackAndQueue
             this.top = null;
         }
 
-        internal void Push(int value)
+        public void Push(int value)
         {
             Node node = new Node(value);
             if (this.top == null)
@@ -22,14 +22,51 @@ namespace StackAndQueue
             this.top = node;
             Console.WriteLine("{0} pushed to stack ", value);
         }
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
         public void Display()
         {
-            Node temp = this.top;
-            while (temp != null)
+            if (top == null)
             {
-                Console.Write(temp.data + " ");
-                temp = temp.next;
+                Console.WriteLine("Stack is Empty");
             }
+            else
+            {
+                Node n = top;
+                while (n.next != null)
+                {
+                    Console.Write(n.data + " ");
+                    n = n.next;
+                }
+                Console.Write(n.data + "\n");
+            }
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            Console.WriteLine("Stack is empty now.");
         }
     }
 }
